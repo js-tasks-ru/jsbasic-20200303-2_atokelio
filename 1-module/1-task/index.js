8d02a0de867669dcf857127a43126f4325d7e6a0
Atokelio <saf-boris@yandex.ru>
2020-03-08 00:56:38 +0300

    add module0, module1 solutions

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -4,5 +4,11 @@
  * @returns {number}
  */
 function factorial(n) {
+  let res = 1;
 
+  for (let i = n; i > 0; i--) {
+    res *= i;
+  }
+
+  return res;
 }
--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -5,5 +5,11 @@
  * @returns {string}
  */
 function truncate(str, maxlength) {
+  const strLength = str.length;
 
+  if (strLength <= maxlength) {
+    return str;
+  } else {
+    return str.slice(0, maxlength - 1) + '…';
+  }
 }
