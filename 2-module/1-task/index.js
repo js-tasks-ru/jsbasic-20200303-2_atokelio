a428f061d9dd701d429fe901f474aaf71184fbda
Atokelio <saf-boris@yandex.ru>
2020-03-11 15:34:01 +0300

    add module 2 tasks

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -4,5 +4,13 @@
  * @returns {Number}
  */
 function sumSalary(salaries) {
+  let bank = 0;
 
+  for (let key in salaries) {
+    if (typeof salaries[key] === 'number') {
+      bank += salaries[key];
+    }
+  }
+
+  return bank;
 }
--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,6 +1,16 @@
 let calculator = {
-  // ваш код
+  read: function (num1, num2) {
+    this.number1 = num1;
+    this.number2 = num2
+  },
+  sum: function () {
+    return this.number1 + this.number2;
+  },
+  mul: function () {
+    return this.number1 * this.number2;
+  }
 };
+
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
 window.calculator = calculator; // делает ваш калькулятор доступным глобально
