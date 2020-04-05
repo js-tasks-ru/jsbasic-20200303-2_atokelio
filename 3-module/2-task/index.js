4b7f838958dd1fc2679e9f72849da3571b5ee954
Boris Safonov <saf-boris@yandex.ru>
2020-04-05 21:47:04 +0300

    add solutions

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -4,4 +4,7 @@
  * @returns {{min:number, max:number}}  объект
  */
 function getMinMax(str) {
+  let numbers = str.match(/-\d+\.\d+|\d+\.\d+|-\d+|\d+/g);
+
+  return { min: Math.min(...numbers), max: Math.max(...numbers) };
 }
